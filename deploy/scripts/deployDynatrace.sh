cf78957c09a7694d32d06d5286d56298d3c5a349
jetzlstorfer <juergen.etzlstorfer@dynatrace.com>
2019-06-16 20:05:35 +0200

    include dt problem notification script

--- deploy/scripts/deployDynatrace.sh
+++ deploy/scripts/deployDynatrace.sh
@@ -38,6 +38,12 @@ print_info "Applying auto tagging rules in Dynatrace."
 verify_install_step $? "Applying auto tagging rules in Dynatrace failed."
 print_info "Applying auto tagging rules in Dynatrace done."
 
+# Setup problem notification in Dynatrace
+print_info "Set up problem notification in Dynatrace."
+./setupProblemNotification.sh $DT_TENANT $DT_API_TOKEN
+verify_install_step $? "Setup of problem notification in Dynatrace failed."
+print_info "Setup of problem notification in Dynatrace done."
+
 # Create secrets to be used by dynatrace-service
 kubectl -n keptn create secret generic dynatrace --from-literal="DT_API_TOKEN=$DT_API_TOKEN" --from-literal="DT_TENANT=$DT_TENANT"
 verify_kubectl $? "Creating dynatrace secret for keptn services failed."
