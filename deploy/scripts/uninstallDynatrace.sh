3d57f1bd7cf813c0f625d83b893c9491462b4534
Johannes Bräuer <johannes.braeuer@dynatrace.com>
2019-07-23 08:26:42 +0200

    Added delete cmd for Cluster Role Binding

--- deploy/scripts/uninstallDynatrace.sh
+++ deploy/scripts/uninstallDynatrace.sh
@@ -3,4 +3,11 @@ echo "Uninstalling Dynatrace from cluster"
 kubectl delete services,deployments,pods --all -n dynatrace --ignore-not-found
 kubectl delete namespace dynatrace --ignore-not-found
 kubectl delete secret dynatrace -n keptn --ignore-not-found
-echo "Uninstalling Dynatrace finished"
+echo "Dynatrace uninstalled"
+
+# Deleting CRB
+DT_OPERATOR_LATEST_RELEASE=$(curl -s https://api.github.com/repos/dynatrace/dynatrace-oneagent-operator/releases/latest | grep tag_name | cut -d '"' -f 4)
+echo "Deleting Cluster Role Binding for Dynatrace Operator $DT_OPERATOR_LATEST_RELEASE"
+
+kubectl delete -f https://raw.githubusercontent.com/Dynatrace/dynatrace-oneagent-operator/$DT_OPERATOR_LATEST_RELEASE/deploy/kubernetes.yaml --ignore-not-found
+echo "Cluster Role Binding for Dynatrace Operator deleted"
