41bda99b989f61e422513ea3918dcf204b4e6868
Christian Kreuzberger <christian.kreuzberger@dynatrace.com>
2020-02-26 12:56:59 +0100

    #107 Refactor sendEvent method for Dynatrace

---
+++ pkg/lib/dynatrace_events.go
@@ -0,0 +1,27 @@
+package lib
+
+import (
+	"encoding/json"
+)
+
+// Sends an event to the Dynatrace events API
+func (dt *DynatraceHelper) SendEvent(dtEvent interface{}) {
+	dt.Logger.Info("Sending event to Dynatrace API")
+
+	jsonString, err := json.Marshal(dtEvent)
+
+	if err != nil {
+		dt.Logger.Error("Error while generating Dynatrace API Request payload.")
+		return
+	}
+
+	body, err := dt.sendDynatraceAPIRequest("/api/v1/events", "POST", string(jsonString))
+
+	if err != nil {
+		dt.Logger.Error("Failed sending Dynatrace API request: " + err.Error())
+		dt.Logger.Error("Response Body:" + body)
+	}
+
+	dt.Logger.Debug("Dynatrace API has accepted the event")
+	dt.Logger.Debug("Response Body:" + body)
+}
