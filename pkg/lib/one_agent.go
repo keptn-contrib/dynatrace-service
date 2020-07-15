bffc56ea72e8882d3ba4c2dc22d81884bbe21768
Johannes <johannes.braeuer@dynatrace.com>
2020-07-15 12:55:18 +0200

    #156 Update of readme

--- pkg/lib/one_agent.go
+++ pkg/lib/one_agent.go
@@ -10,9 +10,10 @@ type dtOperatorReleaseInfo struct {
 
 func (dt *DynatraceHelper) EnsureDTIsInstalled() error {
 	if dt.isDynatraceDeployed() {
-		dt.Logger.Info("Dynatrace OneAgent is installed on cluster")
+		dt.Logger.Info("Dynatrace OneAgent Operator is installed on cluster")
 	} else {
-		dt.Logger.Info("Dynatrace OneAgent is not installed on cluster")
+		dt.Logger.Info("Dynatrace OneAgent Operator is not installed on cluster.")
+		dt.Logger.Info("Please follow the instructions as provided here: https://www.dynatrace.com/support/help/technology-support/cloud-platforms/kubernetes/deploy-oneagent-k8/")
 	}
 	return nil
 }
@@ -20,6 +21,7 @@ func (dt *DynatraceHelper) EnsureDTIsInstalled() error {
 func (dt *DynatraceHelper) isDynatraceDeployed() bool {
 	_, err := dt.KubeApi.AppsV1().Deployments("dynatrace").Get("dynatrace-oneagent-operator", metav1.GetOptions{})
 	if err != nil {
+		dt.Logger.Error(err.Error())
 		return false
 	}
 	return true
