6c90fc16325d1172370438eebdb35b297068c7b0
Johannes <johannes.braeuer@dynatrace.com>
2020-07-17 08:04:46 +0200

    Improved info message and update of release notes

--- pkg/lib/one_agent.go
+++ pkg/lib/one_agent.go
@@ -12,8 +12,12 @@ func (dt *DynatraceHelper) EnsureDTIsInstalled() error {
 	if dt.isDynatraceDeployed() {
 		dt.Logger.Info("Dynatrace OneAgent Operator is installed on cluster")
 	} else {
-		dt.Logger.Info("Dynatrace OneAgent Operator is not installed on cluster.")
-		dt.Logger.Info("Please follow the instructions as provided here: https://www.dynatrace.com/support/help/technology-support/cloud-platforms/kubernetes/deploy-oneagent-k8/")
+		dt.Logger.Info("Dynatrace OneAgent Operator is not installed on cluster")
+		dt.Logger.Info("# ATTENTION # ------------------------------------------------------------------------------------------")
+		dt.Logger.Info("The behavior has changed and Dynatrace OneAgent Operator will NOT be installed.")
+		dt.Logger.Info("If you want to roll-out the OneAgent, please follow the instructions as provided here: ")
+		dt.Logger.Info("https://www.dynatrace.com/support/help/technology-support/cloud-platforms/kubernetes/deploy-oneagent-k8/")
+		dt.Logger.Info("--------------------------------------------------------------------------------------------------------")
 	}
 	return nil
 }
--- releasenotes/releasenotes_develop.md
+++ releasenotes/releasenotes_develop.md
@@ -1,10 +1,16 @@
-# Release Notes 0.7.x
+# Release Notes 0.8.0
 
 ## New Features
 
+- Send problem comments when actions are triggered/finished [#142](https://github.com/keptn-contrib/dynatrace-service/pull/142)
+- Added problemUrl to problem payload [#144](https://github.com/keptn-contrib/dynatrace-service/pull/144)
+- Created und now uses RBAC objects [#145](https://github.com/keptn-contrib/dynatrace-service/pull/145)
+- Removed dependency from keptn-domain ConfigMap [#148](https://github.com/keptn-contrib/dynatrace-service/pull/148)
+- Removed the installation of Dynatrace OneAgent when configuring monitoring [#156](https://github.com/keptn-contrib/dynatrace-service/pull/156)
 
 ## Fixed Issues
 
+- Correct WebSocket URL to /websocket [#152](https://github.com/keptn-contrib/dynatrace-service/pull/152)
 
 ## Known Limitations
 
