eeee9329edfdfab4c35acddf87739eb37bb28da7
Johannes <johannes.braeuer@dynatrace.com>
2020-07-17 09:42:55 +0200

    Improved info message and update of release notes (#160)
    * Improved info message and update of release notes
    
    * Better info message to guide the user

--- pkg/lib/one_agent.go
+++ pkg/lib/one_agent.go
@@ -8,22 +8,22 @@ type dtOperatorReleaseInfo struct {
 	TagName string `json:"tag_name"`
 }
 
-func (dt *DynatraceHelper) EnsureDTIsInstalled() error {
-	if dt.isDynatraceDeployed() {
-		dt.Logger.Info("Dynatrace OneAgent Operator is installed on cluster")
-	} else {
-		dt.Logger.Info("Dynatrace OneAgent Operator is not installed on cluster.")
-		dt.Logger.Info("Please follow the instructions as provided here: https://www.dynatrace.com/support/help/technology-support/cloud-platforms/kubernetes/deploy-oneagent-k8/")
-	}
-	return nil
-}
-
-func (dt *DynatraceHelper) isDynatraceDeployed() bool {
+func (dt *DynatraceHelper) CheckDTIsInstalled() error {
 	_, err := dt.KubeApi.AppsV1().Deployments("dynatrace").Get("dynatrace-oneagent-operator", metav1.GetOptions{})
 	if err != nil {
-		dt.Logger.Error(err.Error())
-		return false
+		dt.Logger.Info(`
+Dynatrace OneAgent Operator is not installed on cluster
+# ATTENTION # ------------------------------------------------------------------------------------
+The behavior has changed and Dynatrace OneAgent Operator will NOT be installed automatically.
+If you want to roll-out the Dynatrace OneAgent Operator: 
+1.) Please follow the instructions as provided here: 
+    https://www.dynatrace.com/support/help/technology-support/cloud-platforms/kubernetes/deploy-oneagent-k8/
+2.) Then, re-deploy the dynatrace-service: 
+    kubectl apply -f https://raw.githubusercontent.com/keptn-contrib/dynatrace-service/<VERSION>/deploy/service.yaml
+--------------------------------------------------------------------------------------------------`)
+	} else {
+		dt.Logger.Info("Dynatrace OneAgent Operator is installed on cluster")
 	}
-	return true
+	return nil
 }
 
