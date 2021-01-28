1ae51e010fe0ebc9deb589d4c3a38369d4996d21
jetzlstorfer <juergen.etzlstorfer@dynatrace.com>
2021-01-28 09:37:54 +0100

    allow environment wide dashboard access
    Signed-off-by: jetzlstorfer <juergen.etzlstorfer@dynatrace.com>

--- pkg/lib/configuration.go
+++ pkg/lib/configuration.go
@@ -40,6 +40,10 @@ func IsServiceSyncEnabled() bool {
 	return readEnvAsBool("SYNCHRONIZE_DYNATRACE_SERVICES", false)
 }
 
+func IsDashboardEnvironmentPublic() bool {
+	return readEnvAsBool("DASHBOARD_ENVIRONMENT_PUBLIC", true)
+}
+
 func readEnvAsBool(env string, fallbackValue bool) bool {
 	if b, err := strconv.ParseBool(os.Getenv(env)); err == nil {
 		return b
