8b38db46e265ef0b4baff46d462aab6cd31b749a
Arthur Pitman <arthur.pitman@dynatrace.com>
2021-06-16 23:21:44 +0200

    Added logging to read env var functions
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>

--- pkg/lib/configuration.go
+++ pkg/lib/configuration.go
@@ -3,6 +3,8 @@ package lib
 import (
 	"os"
 	"strconv"
+
+	log "github.com/sirupsen/logrus"
 )
 
 // IsTaggingRulesGenerationEnabled returns whether tagging rules should be generated when configuring the monitoring
@@ -43,24 +45,55 @@ func IsServiceSyncEnabled() bool {
 // GetServiceSyncInterval returns the number of seconds the service synchronizer should sleep between synchronization runs
 // if the environment variable is empty or cannot be parsed, a default sync interval is used
 func GetServiceSyncInterval() int {
-	const defaultSyncInterval = 300
+	return readEnvAsInt("SYNCHRONIZE_DYNATRACE_SERVICES_INTERVAL_SECONDS", 300)
+}
 
-	intervalEnv := os.Getenv("SYNCHRONIZE_DYNATRACE_SERVICES_INTERVAL_SECONDS")
-	if intervalEnv == "" {
-		return defaultSyncInterval
+func readEnvAsBool(env string, defaultValue bool) bool {
+	envValue := os.Getenv(env)
+	if envValue == "" {
+		log.WithFields(
+			log.Fields{
+				"name":    env,
+				"default": defaultValue,
+			}).Info("Environment variable not set or empty. Using default value.")
+		return defaultValue
 	}
 
-	parseInt, err := strconv.ParseInt(intervalEnv, 10, 32)
+	boolValue, err := strconv.ParseBool(envValue)
 	if err != nil {
-		return defaultSyncInterval
+		log.WithError(err).WithFields(
+			log.Fields{
+				"name":    env,
+				"value":   envValue,
+				"default": defaultValue,
+			}).Error("Unable to parse environment variable. Using default value.")
+		return defaultValue
+	}
+
+	return boolValue
+}
+
+func readEnvAsInt(env string, defaultValue int) int {
+	envValue := os.Getenv(env)
+	if envValue == "" {
+		log.WithFields(
+			log.Fields{
+				"name":    env,
+				"default": defaultValue,
+			}).Info("Environment variable not set or empty. Using default value.")
+		return defaultValue
+	}
+
+	parseInt, err := strconv.ParseInt(envValue, 10, 32)
+	if err != nil {
+		log.WithError(err).WithFields(
+			log.Fields{
+				"name":    env,
+				"value":   envValue,
+				"default": defaultValue,
+			}).Error("Unable to parse environment variable. Using default value.")
+		return defaultValue
 	}
 
 	return int(parseInt)
-}
-
-func readEnvAsBool(env string, fallbackValue bool) bool {
-	if b, err := strconv.ParseBool(os.Getenv(env)); err == nil {
-		return b
-	}
-	return fallbackValue
 }
