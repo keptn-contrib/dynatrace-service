19b7e9146964d39333f836bb88e9106369255901
Arthur Pitman <arthur.pitman@dynatrace.com>
2021-09-08 00:32:26 +0200

    Forward additional problem open fields
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>

--- pkg/event_handler/problem_handler.go
+++ pkg/event_handler/problem_handler.go
@@ -34,12 +34,16 @@ type DTProblemEvent struct {
 		StartTime     int64  `json:"startTime"`
 		Status        string `json:"status"`
 	} `json:"ProblemDetails"`
-	ProblemID    string `json:"ProblemID"`
-	ProblemTitle string `json:"ProblemTitle"`
-	ProblemURL   string `json:"ProblemURL"`
-	State        string `json:"State"`
-	Tags         string `json:"Tags"`
-	EventContext struct {
+	ProblemDetailsHTML string `json:"ProblemDetailsHTML"`
+	ProblemDetailsText string `json:"ProblemDetailsText"`
+	ProblemID          string `json:"ProblemID"`
+	ProblemImpact      string `json:"ProblemImpact"`
+	ProblemSeverity    string `json:"ProblemSeverity"`
+	ProblemTitle       string `json:"ProblemTitle"`
+	ProblemURL         string `json:"ProblemURL"`
+	State              string `json:"State"`
+	Tags               string `json:"Tags"`
+	EventContext       struct {
 		KeptnContext string `json:"keptnContext"`
 		Token        string `json:"token"`
 	} `json:"eventContext"`
@@ -70,11 +74,19 @@ type ProblemDetails struct {
 	ProblemTitle string `json:"ProblemTitle"`
 	// ProblemDetails are all problem event details including root cause
 	ProblemDetails json.RawMessage `json:"ProblemDetails"`
+	// ProblemDetailsHTML are all problem event details including root cause as an HTML-formatted string
+	ProblemDetailsHTML string `json:"ProblemDetailsHTML"`
+	// ProblemDetailsText are all problem event details including root cause as a text-formatted string.
+	ProblemDetailsText string `json:"ProblemDetailsText"`
 	// PID is a unique system identifier of the reported problem.
 	PID string `json:"PID"`
-	// ImpactedEntity is an identifier of the impacted entity
+	// ProblemImpact is the impact level of the problem. Possible values are APPLICATION, SERVICE, or INFRASTRUCTURE.
+	ProblemImpact string `json:"ProblemImpact"`
+	// ProblemSeverity is the severity level of the problem. Possible values are AVAILABILITY, ERROR, PERFORMANCE, RESOURCE_CONTENTION, or CUSTOM_ALERT.const
+	ProblemSeverity string `json:"ProblemSeverity"`
 	// ProblemURL is a back link to the original problem
-	ProblemURL     string `json:"ProblemURL,omitempty"`
+	ProblemURL string `json:"ProblemURL,omitempty"`
+	// ImpactedEntity is an identifier of the impacted entity
 	ImpactedEntity string `json:"ImpactedEntity,omitempty"`
 	// Tags is a comma separated list of tags that are defined for all impacted entities.
 	Tags string `json:"Tags,omitempty"`
@@ -160,14 +172,18 @@ func (eh ProblemEventHandler) handleOpenedProblemFromDT(dtProblemEvent *DTProble
 			Service: service,
 		},
 		Problem: ProblemDetails{
-			State:          "OPEN",
-			PID:            dtProblemEvent.PID,
-			ProblemID:      dtProblemEvent.ProblemID,
-			ProblemTitle:   dtProblemEvent.ProblemTitle,
-			ProblemDetails: json.RawMessage(problemDetailsString),
-			ProblemURL:     dtProblemEvent.ProblemURL,
-			ImpactedEntity: dtProblemEvent.ImpactedEntity,
-			Tags:           dtProblemEvent.Tags,
+			State:              "OPEN",
+			PID:                dtProblemEvent.PID,
+			ProblemID:          dtProblemEvent.ProblemID,
+			ProblemTitle:       dtProblemEvent.ProblemTitle,
+			ProblemDetails:     json.RawMessage(problemDetailsString),
+			ProblemDetailsHTML: dtProblemEvent.ProblemDetailsHTML,
+			ProblemDetailsText: dtProblemEvent.ProblemDetailsText,
+			ProblemImpact:      dtProblemEvent.ProblemImpact,
+			ProblemSeverity:    dtProblemEvent.ProblemSeverity,
+			ProblemURL:         dtProblemEvent.ProblemURL,
+			ImpactedEntity:     dtProblemEvent.ImpactedEntity,
+			Tags:               dtProblemEvent.Tags,
 		},
 	}
 
