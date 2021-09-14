c99ce42d6caf681c3aac4f7559518933f4b2aa1d
Arthur Pitman <arthur.pitman@dynatrace.com>
2021-09-14 14:19:49 +0200

    Add DT_TAGS to custom helm chart
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>

--- gh-actions-scripts/build_helm_chart.sh
+++ gh-actions-scripts/build_helm_chart.sh
@@ -4,9 +4,10 @@
 VERSION=$1
 IMAGE_TAG=$2
 IMAGE=$3
+DT_TAGS=$4
 
-if [ $# -ne 3 ]; then
-  echo "Usage: $0 VERSION IMAGE_TAG IMAGE"
+if [ $# -ne 3 ] && [ $# -ne 4 ]; then
+  echo "Usage: $0 VERSION IMAGE_TAG IMAGE (DT_TAGS)"
   exit
 fi
 
@@ -20,10 +21,19 @@ if [ -z "$IMAGE_TAG" ]; then
   IMAGE_TAG=$VERSION
 fi
 
+if [ -z "$IMAGE" ]; then
+  echo "No image set, exiting..."
+  exit 1
+fi
 
-# replace "appVersion: latest" with "appVersion: $VERSION" in all Chart.yaml files
-find . -name Chart.yaml -exec sed -i -- "s/appVersion: latest/appVersion: ${IMAGE_TAG}/g" {} \;
-find . -name Chart.yaml -exec sed -i -- "s/version: latest/version: ${VERSION}/g" {} \;
+# set  "appVersion" and "version" in Chart.yaml file
+find ./chart -name Chart.yaml -exec sed -i -- "s/appVersion: latest/appVersion: ${IMAGE_TAG}/g" {} \;
+find ./chart -name Chart.yaml -exec sed -i -- "s/version: latest/version: ${VERSION}/g" {} \;
+
+# optionally insert DT_TAGS as environment variable at the end of _dynatrace_service_environment.tpl
+if [ -n "$DT_TAGS" ]; then
+  find ./chart/templates -name _dynatrace_service_environment.tpl -exec sed -i -- "s/{{- end }}/- name: DT_TAGS\n  value: ${DT_TAGS}\n{{- end }}/g" {} \;
+fi
 
 mkdir installer/
 
