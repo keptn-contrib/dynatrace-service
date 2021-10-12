8ea7bc54a81def0a51343dd5d25a2064f1a0aabf
Arthur Pitman <arthur.pitman@dynatrace.com>
2021-10-12 12:54:55 +0200

    Add comments to exported functions
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>

--- internal/sli/unit/mv2_query.go
+++ internal/sli/unit/mv2_query.go
@@ -7,6 +7,7 @@ import (
 
 var mv2Pattern = regexp.MustCompile(`^MV2;(([bB]yte)|([Mm]icro[sS]econd));(.+)$`)
 
+// ParseMV2Query tries to parse a MV2 query into a metrics query and a unit if it allows, otherwise it returns an error
 func ParseMV2Query(mv2Query string) (metricsQuery string, metricUnit string, err error) {
 	chunks := mv2Pattern.FindStringSubmatch(mv2Query)
 	if len(chunks) != 5 {
@@ -30,6 +31,7 @@ func createMV2FormatError(query string) error {
 	return fmt.Errorf("could not parse SLI definition format - should either be 'MV2;Byte;<query>' or 'MV2;MicroSecond;<query>': %s", query)
 }
 
+// ConvertToMV2Query expresses the specified metrics query as a MV2 query if the unit allows, otherwise returns an error
 func ConvertToMV2Query(metricsQuery string, metricUnit string) (string, error) {
 	if canBeConverted(metricUnit) {
 		return fmt.Sprintf("MV2;%s;%s", metricUnit, metricsQuery), nil
