048f3935735282147ba57916dc9e39261619f1f1
Joerg Poecher <j-poecher@users.noreply.github.com>
2021-10-21 18:16:45 +0200

    extends rest client with additional headers and and return values
    Signed-off-by: Joerg Poecher <j-poecher@users.noreply.github.com>

--- internal/rest/rest_client.go
+++ internal/rest/rest_client.go
@@ -13,10 +13,16 @@ import (
 const NoStatus = -1
 
 type ClientInterface interface {
-	Get(apiPath string) ([]byte, int, error)
-	Post(apiPath string, body []byte) ([]byte, int, error)
-	Put(apiPath string, body []byte) ([]byte, int, error)
-	Delete(apiPath string) ([]byte, int, error)
+	Get(apiPath string) ([]byte, int, string, error)
+	Post(apiPath string, body []byte) ([]byte, int, string, error)
+	Put(apiPath string, body []byte) ([]byte, int, string, error)
+	Delete(apiPath string) ([]byte, int, string, error)
+}
+
+type HTTPHeader map[string][]string
+
+func (h HTTPHeader) Add(key string, value string) {
+	h[key] = append(h[key], value)
 }
 
 type ClientError struct {
@@ -29,46 +35,48 @@ func (e *ClientError) Error() string {
 }
 
 type Client struct {
-	httpClient *http.Client
-	baseURL    string
+	httpClient       *http.Client
+	baseURL          string
+	additionalHeader HTTPHeader
 }
 
 // NewClient creates a new Client
-func NewClient(httpClient *http.Client, baseURL string) *Client {
+func NewClient(httpClient *http.Client, baseURL string, additionalHeader HTTPHeader) *Client {
 	return &Client{
-		httpClient: httpClient,
-		baseURL:    baseURL,
+		httpClient:       httpClient,
+		baseURL:          baseURL,
+		additionalHeader: additionalHeader,
 	}
 }
 
 // NewDefaultClient creates a new Client with a default HTTP client set up
-func NewDefaultClient(baseURL string) *Client {
-	return NewClient(&http.Client{}, baseURL)
+func NewDefaultClient(baseURL string, additionalHeader HTTPHeader) *Client {
+	return NewClient(&http.Client{}, baseURL, additionalHeader)
 }
 
-func (c *Client) Get(apiPath string) ([]byte, int, error) {
+func (c *Client) Get(apiPath string) ([]byte, int, string, error) {
 	return c.sendRequest(apiPath, http.MethodGet, nil)
 }
 
-func (c *Client) Post(apiPath string, body []byte) ([]byte, int, error) {
+func (c *Client) Post(apiPath string, body []byte) ([]byte, int, string, error) {
 	return c.sendRequest(apiPath, http.MethodPost, body)
 }
 
-func (c *Client) Put(apiPath string, body []byte) ([]byte, int, error) {
+func (c *Client) Put(apiPath string, body []byte) ([]byte, int, string, error) {
 	return c.sendRequest(apiPath, http.MethodPut, body)
 }
 
-func (c *Client) Delete(apiPath string) ([]byte, int, error) {
+func (c *Client) Delete(apiPath string) ([]byte, int, string, error) {
 	return c.sendRequest(apiPath, http.MethodDelete, nil)
 }
 
 // sendRequest makes an API request and returns the response and the status code or an error
 // the response will not contain any data in case of an error
-func (c *Client) sendRequest(apiPath string, method string, body []byte) ([]byte, int, error) {
+func (c *Client) sendRequest(apiPath string, method string, body []byte) ([]byte, int, string, error) {
 
 	req, err := c.createRequest(apiPath, method, body)
 	if err != nil {
-		return nil, NoStatus, err
+		return nil, NoStatus, "", err
 	}
 
 	return c.doRequest(req)
@@ -91,14 +99,20 @@ func (c *Client) createRequest(apiPath string, method string, body []byte) (*htt
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("User-Agent", "keptn-contrib/dynatrace-service:"+os.Getenv("version"))
 
+	for key, values := range c.additionalHeader {
+		for _, value := range values {
+			req.Header.Add(key, value)
+		}
+	}
+
 	return req, nil
 }
 
 // performs the request and reads the response
-func (c *Client) doRequest(req *http.Request) ([]byte, int, error) {
+func (c *Client) doRequest(req *http.Request) ([]byte, int, string, error) {
 	resp, err := c.httpClient.Do(req)
 	if err != nil {
-		return nil, NoStatus, &ClientError{
+		return nil, NoStatus, "", &ClientError{
 			message: "failed to send request",
 			cause:   err,
 		}
@@ -107,11 +121,11 @@ func (c *Client) doRequest(req *http.Request) ([]byte, int, error) {
 	defer resp.Body.Close()
 	responseBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, NoStatus, &ClientError{
+		return nil, NoStatus, "", &ClientError{
 			message: "failed to read response body",
 			cause:   err,
 		}
 	}
 
-	return responseBody, resp.StatusCode, nil
+	return responseBody, resp.StatusCode, req.URL.String(), nil
 }
