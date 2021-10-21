ed7bcde508bdcd0a1cea10cf2ab24e432e664f57
Arthur Pitman <arthur.pitman@dynatrace.com>
2021-10-21 14:00:35 +0200

    Merge dynatrace API token into credentials
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>

--- internal/credentials/dynatrace_credentials.go
+++ internal/credentials/dynatrace_credentials.go
@@ -2,27 +2,31 @@ package credentials
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 
 	"github.com/keptn-contrib/dynatrace-service/internal/url"
 )
 
+var dynatraceAPITokenRegex = regexp.MustCompile(`^([^\.]+)\.([A-Z0-9]{24})\.([A-Z0-9]{64})$`)
+
 type DynatraceCredentials struct {
 	tenant   string
-	apiToken DynatraceAPIToken
+	apiToken string
 }
 
-func NewDynatraceCredentials(tenant string, apiTokenString string) (*DynatraceCredentials, error) {
+func NewDynatraceCredentials(tenant string, apiToken string) (*DynatraceCredentials, error) {
 	tenant, err := url.MakeCleanURL(tenant)
 	if err != nil {
 		return nil, fmt.Errorf("cannot create Dynatrace credentials: %v", err)
 	}
 
-	apiToken, err := NewDynatraceAPIToken(apiTokenString)
+	apiToken, err = cleanDynatraceAPIToken(apiToken)
 	if err != nil {
 		return nil, fmt.Errorf("cannot create Dynatrace credentials: %v", err)
 	}
 
-	return &DynatraceCredentials{tenant: tenant, apiToken: *apiToken}, nil
+	return &DynatraceCredentials{tenant: tenant, apiToken: apiToken}, nil
 }
 
 // GetTenant gets the base URL of Dynatrace tenant. This is always prefixed with "https://" or "http://".
@@ -30,6 +34,17 @@ func (c *DynatraceCredentials) GetTenant() string {
 	return c.tenant
 }
 
-func (c *DynatraceCredentials) GetAPIToken() DynatraceAPIToken {
+func (c *DynatraceCredentials) GetAPIToken() string {
 	return c.apiToken
+}
+
+func cleanDynatraceAPIToken(t string) (string, error) {
+	t = strings.TrimSpace(t)
+
+	chunks := dynatraceAPITokenRegex.FindStringSubmatch(t)
+	if len(chunks) != 4 {
+		return "", fmt.Errorf("invalid Dynatrace token")
+	}
+
+	return t, nil
 }
---
+++ internal/credentials/dynatrace_credentials_test.go
@@ -0,0 +1,116 @@
+package credentials
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewDynatraceCredentials(t *testing.T) {
+
+	validTestTenant := "https://mySampleEnv.live.dynatrace.com"
+
+	type args struct {
+		tenant   string
+		apiToken string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *DynatraceCredentials
+		wantErr bool
+	}{
+		{
+			name: "valid credentials",
+			args: args{tenant: validTestTenant,
+				apiToken: testDynatraceAPIToken,
+			},
+			want: &DynatraceCredentials{
+				tenant:   validTestTenant,
+				apiToken: testDynatraceAPIToken,
+			},
+			wantErr: false,
+		},
+		{
+			name: "invalid token - empty",
+			args: args{
+				tenant:   validTestTenant,
+				apiToken: "",
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid token - 1 components",
+			args: args{tenant: validTestTenant,
+				apiToken: "dt0c01ST2EY72KQINMH574WMNVI7YNG3DFPBEJYMODIDAEX454M7YWBUVEFOWKPRVMWFASS64NFH52PX6BNDVFFM572RZM",
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid token - 4 components",
+			args: args{
+				tenant:   validTestTenant,
+				apiToken: "dt0c01.ST2EY72KQINMH574WMNVI7YN.G3DFPBEJYMODIDAEX454M7YWBUVEFOWKPRVMWFASS64NFH52PX6BNDVFFM572RZM.AGH",
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid token - bad public characters",
+			args: args{
+				tenant:   validTestTenant,
+				apiToken: "dt0c01.qT2EY72KQINMH574WMNVI7YN.G3DFPBEJYMODIDAEX454M7YWBUVEFOWKPRVMWFASS64NFH52PX6BNDVFFM572RZM",
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid token - public too short",
+			args: args{
+				tenant:   validTestTenant,
+				apiToken: "dt0c01.T2EY72KQINMH574WMNVI7YN.G3DFPBEJYMODIDAEX454M7YWBUVEFOWKPRVMWFASS64NFH52PX6BNDVFFM572RZM",
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid token - public too long",
+			args: args{
+				tenant:   validTestTenant,
+				apiToken: "dt0c01.SST2EY72KQINMH574WMNVI7YN.G3DFPBEJYMODIDAEX454M7YWBUVEFOWKPRVMWFASS64NFH52PX6BNDVFFM572RZM",
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid token - bad secret characters",
+			args: args{tenant: validTestTenant,
+				apiToken: "dt0c01.ST2EY72KQINMH574WMNVI7YN.a3DFPBEJYMODIDAEX454M7YWBUVEFOWKPRVMWFASS64NFH52PX6BNDVFFM572RZM",
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid token - secret too short",
+			args: args{
+				tenant:   validTestTenant,
+				apiToken: "dt0c01.ST2EY72KQINMH574WMNVI7YN.3DFPBEJYMODIDAEX454M7YWBUVEFOWKPRVMWFASS64NFH52PX6BNDVFFM572RZM",
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid token - secret too long",
+			args: args{
+				tenant:   validTestTenant,
+				apiToken: "dt0c01.ST2EY72KQINMH574WMNVI7YN.GG3DFPBEJYMODIDAEX454M7YWBUVEFOWKPRVMWFASS64NFH52PX6BNDVFFM572RZM",
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewDynatraceCredentials(tt.args.tenant, tt.args.apiToken)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewDynatraceCredentials() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewDynatraceCredentials() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
