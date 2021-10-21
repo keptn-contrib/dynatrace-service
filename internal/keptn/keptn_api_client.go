09093670c2d842210034e011e6e97494763bcce6
Joerg Poecher <j-poecher@users.noreply.github.com>
2021-10-21 19:00:02 +0200

    introduces Keptn API client
    Signed-off-by: Joerg Poecher <j-poecher@users.noreply.github.com>

---
+++ internal/keptn/keptn_api_client.go
@@ -0,0 +1,79 @@
+package keptn
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/keptn-contrib/dynatrace-service/internal/rest"
+)
+
+type APIClientInterface interface {
+	Post(apiPath string, body []byte) ([]byte, error)
+}
+
+type APIClient struct {
+	restClient rest.ClientInterface
+}
+
+func NewAPIClient(client rest.ClientInterface) *APIClient {
+	return &APIClient{
+		restClient: client,
+	}
+}
+
+func (c *APIClient) Post(apiPath string, body []byte) ([]byte, error) {
+	body, status, url, err := c.restClient.Post(apiPath, body)
+	if err != nil {
+		return nil, err
+	}
+
+	return validateResponse(body, status, url)
+}
+
+// GenericAPIErrorDTO will support multiple Keptn API errors
+type GenericAPIErrorDTO struct {
+	Code      int    `json:"code"`
+	ErrorCode int    `json:"errorCode"`
+	Message   string `json:"message"`
+}
+
+func (e *GenericAPIErrorDTO) status() int {
+	if e.Code != 0 {
+		return e.Code
+	}
+
+	return e.ErrorCode
+}
+
+type APIError struct {
+	status  int
+	message string
+	url     string
+}
+
+func (e *APIError) Error() string {
+	return fmt.Sprintf("Keptn API error (%d): %s - URL: %s", e.status, e.message, e.url)
+}
+
+func validateResponse(body []byte, status int, url string) ([]byte, error) {
+	if status < 200 || status >= 300 {
+		// try to get the error information
+		keptnAPIError := &GenericAPIErrorDTO{}
+		err := json.Unmarshal(body, keptnAPIError)
+		if err != nil {
+			return body, &APIError{
+				status:  status,
+				message: string(body),
+				url:     url,
+			}
+		}
+
+		return nil, &APIError{
+			status:  keptnAPIError.status(),
+			message: keptnAPIError.Message,
+			url:     url,
+		}
+	}
+
+	return body, nil
+}
