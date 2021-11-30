40fe4720930d9c55f4b2d4228c3a23aedc394653
Arthur Pitman <arthur.pitman@dynatrace.com>
2021-11-30 13:32:20 +0100

    Add test
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>

--- internal/dynatrace/security_problems_client_test.go
+++ internal/dynatrace/security_problems_client_test.go
@@ -24,3 +24,20 @@ func TestSecurityProblemsClient_GetTotalCountByQuery_None(t *testing.T) {
 	assert.NoError(t, err)
 	assert.EqualValues(t, 0, totalSecurityProblemCount)
 }
+
+func TestSecurityProblemsClient_GetTotalCountByQuery_Some(t *testing.T) {
+	handler := test.NewFileBasedURLHandler(t)
+	handler.AddExact("/api/v2/securityProblems?from=1638255600000&to=1638259200000&securityProblemSelector=status(OPEN)", "./testdata/test_securityproblemsclient_gettotalcountbyquery_177.json")
+
+	dtClient, _, teardown := createDynatraceClient(t, handler)
+	defer teardown()
+
+	startTime := time.Unix(1638255600, 0).UTC()
+	endTime := time.Unix(1638259200, 0).UTC()
+	securityProblemQuery := "securityProblemSelector=status(OPEN)"
+
+	totalSecurityProblemCount, err := NewSecurityProblemsClient(dtClient).GetTotalCountByQuery(securityProblemQuery, startTime, endTime)
+
+	assert.NoError(t, err)
+	assert.EqualValues(t, 177, totalSecurityProblemCount)
+}
