17f259f665441459721f94dd7b8932a71f9cd82f
Arthur Pitman <arthur.pitman@dynatrace.com>
2021-12-21 08:25:18 +0100

    Update test
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>

--- internal/dynatrace/slo_client_test.go
+++ internal/dynatrace/slo_client_test.go
@@ -5,12 +5,12 @@ import (
 	"time"
 
 	"github.com/keptn-contrib/dynatrace-service/internal/test"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestExecuteGetDynatraceSLO(t *testing.T) {
 	handler := test.NewFileBasedURLHandler(t)
-	handler.AddStartsWith("/api/v2/slo", "./testdata/test_get_slo_id.json")
-
+	handler.AddExact(SLOPath+"/524ca177-849b-3e8c-8175-42b93fbc33c5?from=1571649084000&to=1571649085000", "./testdata/test_get_slo_id.json")
 	dtClient, _, teardown := createDynatraceClient(t, handler)
 	defer teardown()
 
@@ -19,15 +19,7 @@ func TestExecuteGetDynatraceSLO(t *testing.T) {
 	sloID := "524ca177-849b-3e8c-8175-42b93fbc33c5"
 	sloResult, err := NewSLOClient(dtClient).Get(sloID, startTime, endTime)
 
-	if err != nil {
-		t.Error(err)
-	}
-
-	if sloResult == nil {
-		t.Errorf("No SLO Result returned for " + sloID)
-	}
-
-	if sloResult.EvaluatedPercentage != 95.66405076939219 {
-		t.Error("Not returning expected value for SLO")
-	}
+	assert.NoError(t, err)
+	assert.NotNil(t, sloResult, "No SLO Result returned for "+sloID)
+	assert.EqualValues(t, 95.66405076939219, sloResult.EvaluatedPercentage, "Not returning expected value for SLO")
 }
