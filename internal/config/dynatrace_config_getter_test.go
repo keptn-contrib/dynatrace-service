7675a9d136387cf5955f3af542c520669b18f058
Arthur Pitman <arthur.pitman@dynatrace.com>
2021-12-22 10:32:56 +0100

    Add test
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>

--- internal/config/dynatrace_config_getter_test.go
+++ internal/config/dynatrace_config_getter_test.go
@@ -3,6 +3,8 @@ package config
 import (
 	"testing"
 
+	"github.com/keptn-contrib/dynatrace-service/internal/dynatrace"
+	"github.com/keptn-contrib/dynatrace-service/internal/test"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -79,4 +81,96 @@ dashboard: '****'`,
 			assert.EqualValues(t, tt.want, got)
 		})
 	}
+}
+
+func TestDynatraceConfigGetter_GetDynatraceConfig(t *testing.T) {
+
+	mockEvent := test.EventData{
+		Context:            "01234567-0123-0123-0123-012345678901",
+		Event:              "sh.keptn.event.get-sli.triggered",
+		Source:             "service",
+		Project:            "myproject",
+		Stage:              "mystage",
+		Service:            "myservice",
+		Deployment:         "mydeployment",
+		TestStrategy:       "myteststrategy",
+		DeploymentStrategy: "mydeploymentstrategy",
+		Labels: map[string]string{
+			"dashboard": "12345678-1111-4444-8888-123456789012",
+			"metype":    "SERVICE",
+			"context":   "CONTEXT1",
+			"key":       "special_tag",
+			"value":     "special_value"},
+	}
+	configGetter := NewDynatraceConfigGetter(&dynatraceConfigResourceClientMock{
+		configString: `spec_version: '0.1.0'
+dtCreds: dynatrace-$PROJECT
+dashboard: $LABEL.dashboard
+attachRules:
+  tagRule:
+  - meTypes:
+    - $LABEL.metype
+    tags:
+    - context: CONTEXTLESS
+      key: keptn_project
+      value: $PROJECT
+    - context: CONTEXTLESS
+      key: keptn_service
+      value: $SERVICE
+    - context: CONTEXTLESS
+      key: keptn_stage
+      value: $STAGE
+    - context: $LABEL.context
+      key: $LABEL.key
+      value: $LABEL.value`})
+
+	wantConfig := DynatraceConfig{
+		SpecVersion: "0.1.0",
+		DtCreds:     "dynatrace-myproject",
+		Dashboard:   "12345678-1111-4444-8888-123456789012",
+		AttachRules: &dynatrace.AttachRules{
+			TagRule: []dynatrace.TagRule{
+				dynatrace.TagRule{
+					MeTypes: []string{
+						"SERVICE",
+					},
+					Tags: []dynatrace.TagEntry{
+						dynatrace.TagEntry{
+							Context: "CONTEXTLESS",
+							Key:     "keptn_project",
+							Value:   "myproject",
+						},
+						dynatrace.TagEntry{
+							Context: "CONTEXTLESS",
+							Key:     "keptn_service",
+							Value:   "myservice",
+						},
+						dynatrace.TagEntry{
+							Context: "CONTEXTLESS",
+							Key:     "keptn_stage",
+							Value:   "mystage",
+						},
+						dynatrace.TagEntry{
+							Context: "CONTEXT1",
+							Key:     "special_tag",
+							Value:   "special_value",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	dynatraceConfig, err := configGetter.GetDynatraceConfig(&mockEvent)
+
+	assert.NoError(t, err)
+	assert.EqualValues(t, &wantConfig, dynatraceConfig)
+}
+
+type dynatraceConfigResourceClientMock struct {
+	configString string
+}
+
+func (c *dynatraceConfigResourceClientMock) GetDynatraceConfig(project string, stage string, service string) (string, error) {
+	return c.configString, nil
 }
