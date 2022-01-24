a37b86870dd815b09fe96d56eaac9e69d40920cf
Arthur Pitman <arthur.pitman@dynatrace.com>
2022-01-24 11:01:16 +0100

    Minor renaming
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>

--- internal/sli/v1/common/sli_parser_test.go
+++ internal/sli/v1/common/sli_parser_test.go
@@ -9,28 +9,28 @@ import (
 // TestSLIParser tests parsing of SLIs.
 func TestSLIParser(t *testing.T) {
 	tests := []struct {
-		name                                 string
-		input                                string
-		keyValidator                         KeyValidator
-		expectedQueryParametersAssertionFunc func(t assert.TestingT, q *KeyValuePairs)
-		expectError                          bool
-		expectedErrorMessage                 string
+		name                               string
+		input                              string
+		keyValidator                       KeyValidator
+		expectedKeyValuePairsAssertionFunc func(t assert.TestingT, p *KeyValuePairs)
+		expectError                        bool
+		expectedErrorMessage               string
 	}{
 		{
 			name:         "one key-value, expecting one",
 			input:        "key1=value1",
 			keyValidator: &validatorWithOneKey{},
-			expectedQueryParametersAssertionFunc: func(t assert.TestingT, q *KeyValuePairs) {
-				assert.EqualValues(t, "value1", q.GetValue("key1"))
+			expectedKeyValuePairsAssertionFunc: func(t assert.TestingT, p *KeyValuePairs) {
+				assert.EqualValues(t, "value1", p.GetValue("key1"))
 			},
 		},
 		{
 			name:         "two key-values, expecting two",
 			input:        "key1=value1&key2=value2",
 			keyValidator: &validatorWithTwoKeys{},
-			expectedQueryParametersAssertionFunc: func(t assert.TestingT, q *KeyValuePairs) {
-				assert.EqualValues(t, "value1", q.GetValue("key1"))
-				assert.EqualValues(t, "value2", q.GetValue("key2"))
+			expectedKeyValuePairsAssertionFunc: func(t assert.TestingT, p *KeyValuePairs) {
+				assert.EqualValues(t, "value1", p.GetValue("key1"))
+				assert.EqualValues(t, "value2", p.GetValue("key2"))
 			},
 		},
 
@@ -87,7 +87,7 @@ func TestSLIParser(t *testing.T) {
 			} else {
 				assert.NoError(t, err)
 				if assert.NotNil(t, queryParameters) {
-					tc.expectedQueryParametersAssertionFunc(t, queryParameters)
+					tc.expectedKeyValuePairsAssertionFunc(t, queryParameters)
 				}
 				assert.Empty(t, tc.expectedErrorMessage, "fix test setup")
 			}
--- internal/sli/v1/common/sli_producer_test.go
+++ internal/sli/v1/common/sli_producer_test.go
@@ -9,33 +9,33 @@ import (
 // TestSLIProducer tests SLIProducer.
 func TestSLIProducer(t *testing.T) {
 	tests := []struct {
-		name                 string
-		inputQueryParameters *KeyValuePairs
-		expectedOutput       string
+		name               string
+		inputKeyValuePairs *KeyValuePairs
+		expectedOutput     string
 	}{
 		{
-			name:                 "Empty query parameters",
-			inputQueryParameters: NewKeyValuePairs(map[string]string{}),
+			name:               "Empty query parameters",
+			inputKeyValuePairs: NewKeyValuePairs(map[string]string{}),
 		},
 		{
-			name:                 "One key",
-			inputQueryParameters: NewKeyValuePairs(map[string]string{"key1": "value1"}),
-			expectedOutput:       "key1=value1",
+			name:               "One key",
+			inputKeyValuePairs: NewKeyValuePairs(map[string]string{"key1": "value1"}),
+			expectedOutput:     "key1=value1",
 		},
 		{
-			name:                 "Two keys",
-			inputQueryParameters: NewKeyValuePairs(map[string]string{"key1": "value1", "key2": "value2"}),
-			expectedOutput:       "key1=value1&key2=value2",
+			name:               "Two keys",
+			inputKeyValuePairs: NewKeyValuePairs(map[string]string{"key1": "value1", "key2": "value2"}),
+			expectedOutput:     "key1=value1&key2=value2",
 		},
 		{
-			name:                 "Two keys - potentially different order",
-			inputQueryParameters: NewKeyValuePairs(map[string]string{"key2": "value2", "key1": "value1"}),
-			expectedOutput:       "key1=value1&key2=value2",
+			name:               "Two keys - potentially different order",
+			inputKeyValuePairs: NewKeyValuePairs(map[string]string{"key2": "value2", "key1": "value1"}),
+			expectedOutput:     "key1=value1&key2=value2",
 		},
 	}
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			output := NewSLIProducer(tc.inputQueryParameters).Produce()
+			output := NewSLIProducer(tc.inputKeyValuePairs).Produce()
 			assert.EqualValues(t, tc.expectedOutput, output)
 		})
 	}
