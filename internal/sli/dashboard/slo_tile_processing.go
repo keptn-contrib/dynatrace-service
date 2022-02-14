0f9870a961cbf113c4e8a3c20ce8960b6a8c9ac1
Arthur Pitman <arthur.pitman@dynatrace.com>
2022-02-14 16:27:03 +0100

    fix: Ensure SLO tile processing always produces an indicator (#706)
    * Explicit SLO definition construction
    
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>
    
    * Produce tile on error while generating query
    
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>
    
    * Produce tile result on query error
    
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>
    
    * Remove error from function return
    
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>
    
    * Improve up SLI names
    
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>
    
    * Handle tiles that contain no ID
    
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>
    
    * Add test
    
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>
    
    * Add test
    
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>
    
    * Add test
    
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>
    
    * Update todos
    
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>

--- internal/sli/dashboard/slo_tile_processing.go
+++ internal/sli/dashboard/slo_tile_processing.go
@@ -7,6 +7,7 @@ import (
 	"github.com/keptn-contrib/dynatrace-service/internal/common"
 	"github.com/keptn-contrib/dynatrace-service/internal/dynatrace"
 	"github.com/keptn-contrib/dynatrace-service/internal/sli/v1/slo"
+	keptn "github.com/keptn/go-utils/pkg/lib"
 	keptnv2 "github.com/keptn/go-utils/pkg/lib/v0_2_0"
 	log "github.com/sirupsen/logrus"
 )
@@ -26,67 +27,89 @@ func NewSLOTileProcessing(client dynatrace.ClientInterface, startUnix time.Time,
 }
 
 func (p *SLOTileProcessing) Process(tile *dynatrace.Tile) []*TileResult {
-	// we will take the SLO definition from Dynatrace
-	var results []*TileResult
-
-	for _, sloEntity := range tile.AssignedEntities {
-		log.WithField("sloEntity", sloEntity).Debug("Processing SLO Definition")
-
-		tileResult, err := p.processSLOTile(sloEntity, p.startUnix, p.endUnix)
-		if err != nil {
-			log.WithError(err).Error("Error Processing SLO")
-			continue
-		}
-
-		results = append(results, tileResult)
+	if len(tile.AssignedEntities) == 0 {
+		indicatorName := "slo_tile_without_slo"
+		return []*TileResult{&TileResult{
+			sliResult: &keptnv2.SLIResult{
+				Metric:  indicatorName,
+				Success: false,
+				Message: "SLO tile contains no SLO IDs",
+			},
+			sliName: indicatorName,
+		}}
 	}
 
+	var results []*TileResult
+	for _, sloID := range tile.AssignedEntities {
+		log.WithField("sloEntity", sloID).Debug("Processing SLO Definition")
+		results = append(results, p.processSLO(sloID, p.startUnix, p.endUnix))
+	}
 	return results
 }
 
-// processSLOTile Processes an SLO Tile and queries the data from the Dynatrace API.
-// If successful returns sliResult, sliIndicatorName, sliQuery & sloDefinition
-func (p *SLOTileProcessing) processSLOTile(sloID string, startUnix time.Time, endUnix time.Time) (*TileResult, error) {
-
-	// Step 1: Query the Dynatrace API to get the actual value for this sloID
-	sloResult, err := dynatrace.NewSLOClient(p.client).Get(dynatrace.NewSLOClientGetParameters(sloID, startUnix, endUnix))
+// processSLO processes an SLO by querying the data from the Dynatrace API.
+// Returns a TileResult with sliResult, sliIndicatorName, sliQuery & sloDefinition
+func (p *SLOTileProcessing) processSLO(sloID string, startUnix time.Time, endUnix time.Time) *TileResult {
+	query, err := slo.NewQuery(sloID)
 	if err != nil {
-		return nil, err
+		// TODO: 2021-02-14: Check that this indicator name still aligns with all possible errors.
+		indicatorName := "slo_without_id"
+		return &TileResult{
+			sliResult: &keptnv2.SLIResult{
+				Metric:  indicatorName,
+				Success: false,
+				Message: err.Error(),
+			},
+			sliName: indicatorName,
+		}
 	}
 
-	// Step 2: As we have the SLO Result including SLO Definition we add it to the SLI & SLO objects
-	// IndicatorName is based on the slo Name
-	// the value defaults to the E
+	// Step 1: Query the Dynatrace API to get the actual value for this sloID
+	sloResult, err := dynatrace.NewSLOClient(p.client).Get(dynatrace.NewSLOClientGetParameters(query.GetSLOID(), startUnix, endUnix))
+	if err != nil {
+		indicatorName := common.CleanIndicatorName("slo_" + sloID)
+		return &TileResult{
+			sliResult: &keptnv2.SLIResult{
+				Metric:  indicatorName,
+				Success: false,
+				Message: err.Error(),
+			},
+			sliName: indicatorName,
+		}
+	}
+
+	// Step 2: Transform the SLO result into an SLI result and SLO definition
+	// IndicatorName is based on the SLO Name
 	indicatorName := common.CleanIndicatorName(sloResult.Name)
-	value := sloResult.EvaluatedPercentage
 	sliResult := &keptnv2.SLIResult{
 		Metric:  indicatorName,
-		Value:   value,
+		Value:   sloResult.EvaluatedPercentage,
 		Success: true,
 	}
 
 	log.WithFields(
 		log.Fields{
 			"indicatorName": indicatorName,
-			"value":         value,
+			"value":         sloResult.EvaluatedPercentage,
 		}).Debug("Adding SLO to sloResult")
-
-	query, err := slo.NewQuery(sloID)
-	if err != nil {
-		return nil, err
-	}
-
 	// TODO: 2021-12-20: check: maybe in the future we will allow users to add additional SLO defs via the Tile Name, e.g: weight or KeySli
 
 	// see https://github.com/keptn-contrib/dynatrace-sli-service/issues/97#issuecomment-766110172 for explanation about mappings to pass and warning
-	sloString := fmt.Sprintf("sli=%s;pass=>=%f;warning=>=%f", indicatorName, sloResult.Warning, sloResult.Target)
-	sloDefinition := common.ParsePassAndWarningWithoutDefaultsFrom(sloString)
+	passCriterion := keptn.SLOCriteria{Criteria: []string{fmt.Sprintf(">=%f", sloResult.Warning)}}
+	warningCriterion := keptn.SLOCriteria{Criteria: []string{fmt.Sprintf(">=%f", sloResult.Target)}}
+
+	sloDefinition := &keptn.SLO{
+		SLI:     indicatorName,
+		Pass:    []*keptn.SLOCriteria{&passCriterion},
+		Warning: []*keptn.SLOCriteria{&warningCriterion},
+		Weight:  1,
+		KeySLI:  false,
+	}
 
 	return &TileResult{
 		sliResult: sliResult,
 		objective: sloDefinition,
 		sliName:   indicatorName,
 		sliQuery:  slo.NewQueryProducer(*query).Produce(),
-	}, nil
-
+	}
 }
