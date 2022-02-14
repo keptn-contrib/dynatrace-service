048b080b7b49d0f50f97a7ef054a84e797647249
Arthur Pitman <arthur.pitman@dynatrace.com>
2022-02-14 17:36:27 +0100

    Produce tile result on query error
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>

--- internal/sli/dashboard/problem_tile_processing.go
+++ internal/sli/dashboard/problem_tile_processing.go
@@ -11,6 +11,8 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+const problemsIndicatorName = "problems"
+
 type ProblemTileProcessing struct {
 	client    dynatrace.ClientInterface
 	startUnix time.Time
@@ -46,44 +48,43 @@ func (p *ProblemTileProcessing) Process(tile *dynatrace.Tile, dashboardFilter *d
 // If successful returns sliResult, sliIndicatorName, sliQuery & sloDefinition
 func (p *ProblemTileProcessing) processOpenProblemTile(query problems.Query, startUnix time.Time, endUnix time.Time) (*TileResult, error) {
 
-	// Step 1: Query the Dynatrace API to get the number of actual problems matching that query and timeframe
-	totalProblemCount, err := dynatrace.NewProblemsV2Client(p.client).GetTotalCountByQuery(dynatrace.NewProblemsV2ClientQueryParameters(query, startUnix, endUnix))
-	if err != nil {
-		return nil, err
-	}
-
-	// Step 2: As we have the SLO Result including SLO Definition we add it to the SLI & SLO objects
-	// IndicatorName is based on the slo Name
-	// the value defaults to the E
-	indicatorName := "problems"
-	value := float64(totalProblemCount)
-	sliResult := &keptnv2.SLIResult{
-		Metric:  indicatorName,
-		Value:   value,
-		Success: true,
-	}
+	sliResult := p.getProblemCountAsSLIResult(query)
 
 	log.WithFields(
 		log.Fields{
-			"indicatorName": indicatorName,
-			"value":         value,
+			"indicatorName": problemsIndicatorName,
+			"value":         sliResult.Value,
 		}).Debug("Adding SLO to sloResult")
-
-	// add this to our SLI Indicator JSON in case we need to generate an SLI.yaml
-	sliQuery := problemsv2.NewQueryProducer(query).Produce()
 
 	// TODO: 2022-02-14: check: maybe in the future we will allow users to add additional SLO defs via the tile name, e.g. weight or KeySli.
 	sloDefinition := &keptn.SLO{
-		SLI:    indicatorName,
+		SLI:    problemsIndicatorName,
 		Pass:   []*keptn.SLOCriteria{{Criteria: []string{"<=0"}}},
 		Weight: 1,
 		KeySLI: true,
 	}
 
 	return &TileResult{
-		sliResult: sliResult,
+		sliResult: &sliResult,
 		objective: sloDefinition,
-		sliName:   indicatorName,
-		sliQuery:  sliQuery,
+		sliName:   problemsIndicatorName,
+		sliQuery:  problemsv2.NewQueryProducer(query).Produce(),
 	}, nil
+}
+
+func (p *ProblemTileProcessing) getProblemCountAsSLIResult(query problems.Query) keptnv2.SLIResult {
+	totalProblemCount, err := dynatrace.NewProblemsV2Client(p.client).GetTotalCountByQuery(dynatrace.NewProblemsV2ClientQueryParameters(query, p.startUnix, p.endUnix))
+	if err != nil {
+		return keptnv2.SLIResult{
+			Metric:  problemsIndicatorName,
+			Success: false,
+			Message: err.Error(),
+		}
+	}
+
+	return keptnv2.SLIResult{
+		Metric:  problemsIndicatorName,
+		Value:   float64(totalProblemCount),
+		Success: true,
+	}
 }
