8f25888df63b7a9677b7f2e379cd347dffeaac43
Arthur Pitman <arthur.pitman@dynatrace.com>
2022-02-15 16:02:24 +0100

    Introduce helper functions
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>

--- internal/sli/dashboard/tile_result.go
+++ internal/sli/dashboard/tile_result.go
@@ -11,3 +11,41 @@ type TileResult struct {
 	sliName   string
 	sliQuery  string
 }
+
+func newUnsuccessfulTileResult(indicatorName string, message string) TileResult {
+	return TileResult{
+		sliResult: &keptnv2.SLIResult{
+			Metric:  indicatorName,
+			Success: false,
+			Message: message,
+		},
+		sliName: indicatorName,
+	}
+}
+
+func newUnsuccessfulTileResultFromSLODefinition(sloDefinition *keptnapi.SLO, message string) TileResult {
+	return TileResult{
+		sliResult: &keptnv2.SLIResult{
+			Metric:  sloDefinition.SLI,
+			Value:   0,
+			Success: false,
+			Message: message,
+		},
+		objective: sloDefinition,
+		sliName:   sloDefinition.SLI,
+	}
+}
+
+func newUnsuccessfulTileResultFromSLODefinitionAndSLIQuery(sloDefinition *keptnapi.SLO, sliQuery string, message string) TileResult {
+	return TileResult{
+		sliResult: &keptnv2.SLIResult{
+			Metric:  sloDefinition.SLI,
+			Value:   0,
+			Success: false,
+			Message: message,
+		},
+		objective: sloDefinition,
+		sliName:   sloDefinition.SLI,
+		sliQuery:  sliQuery,
+	}
+}
