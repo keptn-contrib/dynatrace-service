b2c381bab00619e445abd586061b968e757a2536
Arthur Pitman <arthur.pitman@dynatrace.com>
2022-04-22 11:25:44 +0200

    Linting
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>

--- internal/keptn/keptn_api_client.go
+++ internal/keptn/keptn_api_client.go
@@ -7,30 +7,25 @@ import (
 	"github.com/keptn-contrib/dynatrace-service/internal/rest"
 )
 
+// APIClientInterface provides methods for accessing a Keptn API.
 type APIClientInterface interface {
-	Get(apiPath string) ([]byte, error)
+	// Post performs a post request and returns a validated response or an error.
 	Post(apiPath string, body []byte) ([]byte, error)
 }
 
+// APIClient is an implementation of APIClientInterface using a rest.ClientInterface.
 type APIClient struct {
 	restClient rest.ClientInterface
 }
 
+// NewAPIClient creates a new APIClient.
 func NewAPIClient(client rest.ClientInterface) *APIClient {
 	return &APIClient{
 		restClient: client,
 	}
 }
 
-func (c *APIClient) Get(apiPath string) ([]byte, error) {
-	body, status, url, err := c.restClient.Get(apiPath)
-	if err != nil {
-		return nil, err
-	}
-
-	return validateResponse(body, status, url)
-}
-
+// Post performs a post request and returns a validated response or an error.
 func (c *APIClient) Post(apiPath string, body []byte) ([]byte, error) {
 	body, status, url, err := c.restClient.Post(apiPath, body)
 	if err != nil {
@@ -55,12 +50,14 @@ func (e *genericAPIErrorDTO) status() int {
 	return e.ErrorCode
 }
 
+// APIError respresents an error returned from a Keptn API.
 type APIError struct {
 	status  int
 	message string
 	url     string
 }
 
+// Error returns a string representation of the APIError.
 func (e *APIError) Error() string {
 	return fmt.Sprintf("Keptn API error (%d): %s - URL: %s", e.status, e.message, e.url)
 }
