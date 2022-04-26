0a0ccc32f604ac16fe3e6e46d4c45212e83686cb
Arthur Pitman <arthur.pitman@dynatrace.com>
2022-04-26 10:06:36 +0200

    Move constant
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>

--- internal/action/common.go
+++ internal/action/common.go
@@ -6,6 +6,7 @@ import (
 )
 
 const eventSource = "Keptn dynatrace-service"
+const bridgeURLKey = "Keptns Bridge"
 
 func createCustomProperties(a adapter.EventContentAdapter, imageAndTag common.ImageAndTag, bridgeURL string) map[string]string {
 	customProperties := map[string]string{
@@ -25,7 +26,7 @@ func createCustomProperties(a adapter.EventContentAdapter, imageAndTag common.Im
 	}
 
 	if bridgeURL != "" {
-		customProperties[common.BridgeLabel] = bridgeURL
+		customProperties[bridgeURLKey] = bridgeURL
 	}
 
 	return customProperties
