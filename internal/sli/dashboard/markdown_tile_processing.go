eb315fe21a20529a517d78bea45f7ac6f61ae31b
Joerg Poecher <j-poecher@users.noreply.github.com>
2022-05-09 18:25:19 +0200

    parsing markdown now returns errors; split up tests
    Signed-off-by: Joerg Poecher <j-poecher@users.noreply.github.com>

--- internal/sli/dashboard/markdown_tile_processing.go
+++ internal/sli/dashboard/markdown_tile_processing.go
@@ -1,11 +1,40 @@
 package dashboard
 
 import (
-	"github.com/keptn-contrib/dynatrace-service/internal/dynatrace"
-	keptncommon "github.com/keptn/go-utils/pkg/lib"
+	"fmt"
 	"strconv"
 	"strings"
+
+	keptncommon "github.com/keptn/go-utils/pkg/lib"
+
+	"github.com/keptn-contrib/dynatrace-service/internal/dynatrace"
 )
+
+type markdownParsingErrors struct {
+	errors []error
+}
+
+func (err *markdownParsingErrors) Error() string {
+	var errStrings = make([]string, len(err.errors))
+	for i, e := range err.errors {
+		errStrings[i] = e.Error()
+	}
+	return strings.Join(errStrings, ";")
+}
+
+type invalidValueError struct {
+	key   string
+	value string
+}
+
+func (err *invalidValueError) Error() string {
+	return fmt.Sprintf("invalid value for '%s': %s", err.key, err.value)
+}
+
+type markdownParsingResult struct {
+	totalScore keptncommon.SLOScore
+	comparison keptncommon.SLOComparison
+}
 
 type MarkdownTileProcessing struct {
 }
@@ -16,17 +45,41 @@ func NewMarkdownTileProcessing() *MarkdownTileProcessing {
 }
 
 // Process will overwrite the default values for SLOScore and SLOComparison with the contents found in the markdown
-func (p *MarkdownTileProcessing) Process(tile *dynatrace.Tile, defaultScore keptncommon.SLOScore, defaultComparison keptncommon.SLOComparison) (*keptncommon.SLOScore, *keptncommon.SLOComparison) {
+func (p *MarkdownTileProcessing) Process(tile *dynatrace.Tile, defaultScore keptncommon.SLOScore, defaultComparison keptncommon.SLOComparison) (*markdownParsingResult, error) {
 	// we allow the user to use a markdown to specify SLI/SLO properties, e.g: KQG.Total.Pass
 	// if we find KQG. we process the markdown
 	return parseMarkdownConfiguration(tile.Markdown, defaultScore, defaultComparison)
 }
 
+const (
+	totalPass                  = "kqg.total.pass"
+	totalWarning               = "kqg.total.warning"
+	compareWithScore           = "kqg.compare.withscore"
+	compareWithScoreAll        = "all"
+	compareWithScorePass       = "pass"
+	compareWithScorePassOrWarn = "pass_or_warn"
+	compareResults             = "kqg.compare.results" // this is an int! You cannot specify 'single_result' or 'several results' at all -> it is derived from the number of results
+	compareResultsSingle       = "single_result"
+	compareResultsMultiple     = "several_results"
+	compareFunction            = "kqg.compare.function"
+	compareFunctionAvg         = "avg"
+	compareFunctionP50         = "p50"
+	compareFunctionP90         = "p90"
+	compareFunctionP95         = "p95"
+)
+
 // parseMarkdownConfiguration parses a text that can be used in a Markdown tile to specify global SLO properties
-func parseMarkdownConfiguration(markdown string, totalScore keptncommon.SLOScore, comparison keptncommon.SLOComparison) (*keptncommon.SLOScore, *keptncommon.SLOComparison) {
+func parseMarkdownConfiguration(markdown string, totalScore keptncommon.SLOScore, comparison keptncommon.SLOComparison) (*markdownParsingResult, error) {
 	if !strings.Contains(markdown, "KQG.") {
 		return nil, nil
 	}
+
+	result := &markdownParsingResult{
+		totalScore: totalScore,
+		comparison: comparison,
+	}
+
+	var errs []error
 
 	markdownSplits := strings.Split(markdown, ";")
 	for _, markdownSplitValue := range markdownSplits {
@@ -35,42 +88,77 @@ func parseMarkdownConfiguration(markdown string, totalScore keptncommon.SLOScore
 			continue
 		}
 
-		// lets get configname and value
-		configName := strings.ToLower(configValueSplits[0])
-		configValue := configValueSplits[1]
+		// lets separate key and value
+		key := strings.ToLower(configValueSplits[0])
+		value := configValueSplits[1]
 
-		switch configName {
-		case "kqg.total.pass":
-			totalScore.Pass = configValue
-		case "kqg.total.warning":
-			totalScore.Warning = configValue
-		case "kqg.compare.withscore":
-			comparison.IncludeResultWithScore = configValue
-			if (configValue == "pass") || (configValue == "pass_or_warn") || (configValue == "all") {
-				comparison.IncludeResultWithScore = configValue
-			} else {
-				comparison.IncludeResultWithScore = "pass"
-			}
-		case "kqg.compare.results":
-			noresults, err := strconv.Atoi(configValue)
+		switch key {
+		case totalPass:
+			result.totalScore.Pass = value
+		case totalWarning:
+			result.totalScore.Warning = value
+		case compareWithScore:
+			score, err := parseCompareWithScore(value)
 			if err != nil {
-				comparison.NumberOfComparisonResults = 1
-			} else {
-				comparison.NumberOfComparisonResults = noresults
+				errs = append(errs, err)
 			}
-			if comparison.NumberOfComparisonResults > 1 {
-				comparison.CompareWith = "several_results"
-			} else {
-				comparison.CompareWith = "single_result"
+			result.comparison.IncludeResultWithScore = score
+		case compareResults:
+			numberOfResults, err := parseCompareNumberOfResults(value)
+			if err != nil {
+				errs = append(errs, err)
 			}
-		case "kqg.compare.function":
-			if (configValue == "avg") || (configValue == "p50") || (configValue == "p90") || (configValue == "p95") {
-				comparison.AggregateFunction = configValue
-			} else {
-				comparison.AggregateFunction = "avg"
+			result.comparison.NumberOfComparisonResults = numberOfResults
+		case compareFunction:
+			aggregateFunc, err := parseAggregateFunction(value)
+			if err != nil {
+				errs = append(errs, err)
 			}
+			result.comparison.AggregateFunction = aggregateFunc
 		}
 	}
 
-	return &totalScore, &comparison
+	if len(errs) > 0 {
+		return nil, &markdownParsingErrors{
+			errors: errs,
+		}
+	}
+
+	result.comparison.CompareWith = compareResultsSingle
+	if result.comparison.NumberOfComparisonResults > 1 {
+		result.comparison.CompareWith = compareResultsMultiple
+	}
+
+	return result, nil
+}
+
+func parseCompareWithScore(value string) (string, error) {
+	switch value {
+	case compareWithScorePass, compareWithScoreAll, compareWithScorePassOrWarn:
+		return value, nil
+	}
+
+	return "", &invalidValueError{key: compareWithScore, value: value}
+}
+
+func parseCompareNumberOfResults(value string) (int, error) {
+	numberOfResults, err := strconv.Atoi(value)
+	if err != nil {
+		return 0, &invalidValueError{key: compareResults, value: value}
+	}
+
+	if numberOfResults < 1 {
+		return 0, &invalidValueError{key: compareResults, value: value}
+	}
+
+	return numberOfResults, nil
+}
+
+func parseAggregateFunction(value string) (string, error) {
+	switch value {
+	case compareFunctionAvg, compareFunctionP50, compareFunctionP90, compareFunctionP95:
+		return value, nil
+	}
+
+	return "", &invalidValueError{key: compareFunction, value: value}
 }
--- internal/sli/dashboard/markdown_tile_processing_test.go
+++ internal/sli/dashboard/markdown_tile_processing_test.go
@@ -1,88 +1,115 @@
 package dashboard
 
 import (
+	"testing"
+
 	keptnapi "github.com/keptn/go-utils/pkg/lib"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
-func TestParseMarkdownConfigurationParams(t *testing.T) {
+func TestParseMarkdownConfigurationParams_SuccessCases(t *testing.T) {
 	testConfigs := []struct {
+		name               string
 		input              string
-		expectedScore      *keptnapi.SLOScore
-		expectedComparison *keptnapi.SLOComparison
+		expectedScore      keptnapi.SLOScore
+		expectedComparison keptnapi.SLOComparison
 	}{
-		// single result
 		{
-			"KQG.Total.Pass=90%;KQG.Total.Warning=70%;KQG.Compare.WithScore=pass;KQG.Compare.Results=1;KQG.Compare.Function=avg",
-			createSLOScore("90%", "70%"),
-			createSLOComparison("single_result", "pass", 1, "avg"),
+			name:               "single result",
+			input:              "KQG.Total.Pass=90%;KQG.Total.Warning=70%;KQG.Compare.WithScore=pass;KQG.Compare.Results=1;KQG.Compare.Function=avg",
+			expectedScore:      createSLOScore("90%", "70%"),
+			expectedComparison: createSLOComparison("single_result", "pass", 1, "avg"),
 		},
-		// several results, p50
+		//
 		{
-			"KQG.Total.Pass=91%;KQG.Total.Warning=71%;KQG.Compare.WithScore=pass;KQG.Compare.Results=2;KQG.Compare.Function=p50",
-			createSLOScore("91%", "71%"),
-			createSLOComparison("several_results", "pass", 2, "p50"),
+			name:               "several results p50",
+			input:              "KQG.Total.Pass=91%;KQG.Total.Warning=71%;KQG.Compare.WithScore=pass;KQG.Compare.Results=2;KQG.Compare.Function=p50",
+			expectedScore:      createSLOScore("91%", "71%"),
+			expectedComparison: createSLOComparison("several_results", "pass", 2, "p50"),
 		},
-		// several results, p90
+		//
 		{
-			"KQG.Total.Pass=92%;KQG.Total.Warning=72%;KQG.Compare.WithScore=pass;KQG.Compare.Results=3;KQG.Compare.Function=p90",
-			createSLOScore("92%", "72%"),
-			createSLOComparison("several_results", "pass", 3, "p90"),
+			name:               "several results p90",
+			input:              "KQG.Total.Pass=92%;KQG.Total.Warning=72%;KQG.Compare.WithScore=pass;KQG.Compare.Results=3;KQG.Compare.Function=p90",
+			expectedScore:      createSLOScore("92%", "72%"),
+			expectedComparison: createSLOComparison("several_results", "pass", 3, "p90"),
 		},
-		// several results, p95
+		//
 		{
-			"KQG.Total.Pass=93%;KQG.Total.Warning=73%;KQG.Compare.WithScore=pass;KQG.Compare.Results=4;KQG.Compare.Function=p95",
-			createSLOScore("93%", "73%"),
-			createSLOComparison("several_results", "pass", 4, "p95"),
+			name:               "several results p95",
+			input:              "KQG.Total.Pass=93%;KQG.Total.Warning=73%;KQG.Compare.WithScore=pass;KQG.Compare.Results=4;KQG.Compare.Function=p95",
+			expectedScore:      createSLOScore("93%", "73%"),
+			expectedComparison: createSLOComparison("several_results", "pass", 4, "p95"),
 		},
-		// several results, p95, all
+		//
 		{
-			"KQG.Total.Pass=94%;KQG.Total.Warning=74%;KQG.Compare.WithScore=all;KQG.Compare.Results=5;KQG.Compare.Function=p95",
-			createSLOScore("94%", "74%"),
-			createSLOComparison("several_results", "all", 5, "p95"),
+			name:               "several results p95 all",
+			input:              "KQG.Total.Pass=94%;KQG.Total.Warning=74%;KQG.Compare.WithScore=all;KQG.Compare.Results=5;KQG.Compare.Function=p95",
+			expectedScore:      createSLOScore("94%", "74%"),
+			expectedComparison: createSLOComparison("several_results", "all", 5, "p95"),
 		},
-		// several results, p95, pass_or_warn
+		//
 		{
-			"KQG.Total.Pass=95%;KQG.Total.Warning=75%;KQG.Compare.WithScore=pass_or_warn;KQG.Compare.Results=6;KQG.Compare.Function=p95",
-			createSLOScore("95%", "75%"),
-			createSLOComparison("several_results", "pass_or_warn", 6, "p95"),
-		},
-		// several results, p95, fallback to pass if compare function is unknown
-		{
-			"KQG.Total.Pass=96%;KQG.Total.Warning=76%;KQG.Compare.WithScore=warn;KQG.Compare.Results=7;KQG.Compare.Function=p95",
-			createSLOScore("96%", "76%"),
-			createSLOComparison("several_results", "pass", 7, "p95"),
-		},
-		// several results, fallback if function is unknown e.g. p97
-		{
-			"KQG.Total.Pass=97%;KQG.Total.Warning=77%;KQG.Compare.WithScore=pass;KQG.Compare.Results=8;KQG.Compare.Function=p97",
-			createSLOScore("97%", "77%"),
-			createSLOComparison("several_results", "pass", 8, "avg"),
-		},
-		// several results, fallback if function is unknown e.g. p97, ignore dashboard query behaviour
-		{
-			"KQG.Total.Pass=98%;KQG.Total.Warning=78%;KQG.Compare.WithScore=pass;KQG.Compare.Results=9;KQG.Compare.Function=p97;KQG.QueryBehavior=ParseOnChange",
-			createSLOScore("98%", "78%"),
-			createSLOComparison("several_results", "pass", 9, "avg"),
+			name:               "several results p95 pass_or_warn",
+			input:              "KQG.Total.Pass=95%;KQG.Total.Warning=75%;KQG.Compare.WithScore=pass_or_warn;KQG.Compare.Results=6;KQG.Compare.Function=p95",
+			expectedScore:      createSLOScore("95%", "75%"),
+			expectedComparison: createSLOComparison("several_results", "pass_or_warn", 6, "p95"),
 		},
 	}
 	for _, config := range testConfigs {
-		actualScore, actualComparison := parseMarkdownConfiguration(config.input, createDefaultSLOScore(), createDefaultSLOComparison())
-
-		assert.EqualValues(t, config.expectedScore, actualScore)
-		assert.EqualValues(t, config.expectedComparison, actualComparison)
+		t.Run(config.name, func(t *testing.T) {
+			result, err := parseMarkdownConfiguration(config.input, createDefaultSLOScore(), createDefaultSLOComparison())
+			if assert.NoError(t, err) {
+				assert.EqualValues(t, config.expectedScore, result.totalScore)
+				assert.EqualValues(t, config.expectedComparison, result.comparison)
+			}
+		})
 	}
 }
 
-func createSLOScore(pass string, warning string) *keptnapi.SLOScore {
-	return &keptnapi.SLOScore{
+func TestParseMarkdownConfigurationParams_ErrorCases(t *testing.T) {
+	testConfigs := []struct {
+		name             string
+		input            string
+		expectedMessages []string
+	}{
+		{
+			name:             "unknown compare with score function",
+			input:            "KQG.Total.Pass=96%;KQG.Total.Warning=76%;KQG.Compare.WithScore=warn;KQG.Compare.Results=7;KQG.Compare.Function=p95",
+			expectedMessages: []string{"kqg.compare.withscore", "warn"},
+		},
+		{
+			name:             "unknown compare function, p97",
+			input:            "KQG.Total.Pass=97%;KQG.Total.Warning=77%;KQG.Compare.WithScore=pass;KQG.Compare.Results=8;KQG.Compare.Function=p97",
+			expectedMessages: []string{"kqg.compare.function", "p97"},
+		},
+		{
+			name:             "wrong number of results, 0",
+			input:            "KQG.Total.Pass=97%;KQG.Total.Warning=77%;KQG.Compare.WithScore=pass;KQG.Compare.Results=0;KQG.Compare.Function=p95",
+			expectedMessages: []string{"kqg.compare.results", "0"},
+		},
+	}
+	for _, config := range testConfigs {
+		t.Run(config.name, func(t *testing.T) {
+			result, err := parseMarkdownConfiguration(config.input, createDefaultSLOScore(), createDefaultSLOComparison())
+			if assert.Error(t, err) {
+				assert.Nil(t, result)
+				for _, msg := range config.expectedMessages {
+					assert.Contains(t, err.Error(), msg)
+				}
+			}
+		})
+	}
+}
+
+func createSLOScore(pass string, warning string) keptnapi.SLOScore {
+	return keptnapi.SLOScore{
 		Pass:    pass,
 		Warning: warning,
 	}
 }
-func createSLOComparison(compareWith string, include string, numberOfResults int, aggregateFunc string) *keptnapi.SLOComparison {
-	return &keptnapi.SLOComparison{
+func createSLOComparison(compareWith string, include string, numberOfResults int, aggregateFunc string) keptnapi.SLOComparison {
+	return keptnapi.SLOComparison{
 		CompareWith:               compareWith,
 		IncludeResultWithScore:    include,
 		NumberOfComparisonResults: numberOfResults,
