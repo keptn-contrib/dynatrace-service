57504fe6f2d74fe6615d0955fa26f45baad9fbd7
Joerg Poecher <j-poecher@users.noreply.github.com>
2022-06-02 09:21:55 +0200

    fix: description cannot be empty for event in release triggered handler
    Signed-off-by: Joerg Poecher <j-poecher@users.noreply.github.com>

--- internal/action/release_triggered_event_handler.go
+++ internal/action/release_triggered_event_handler.go
@@ -4,11 +4,12 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/keptn-contrib/dynatrace-service/internal/dynatrace"
-	"github.com/keptn-contrib/dynatrace-service/internal/keptn"
 	keptnevents "github.com/keptn/go-utils/pkg/lib"
 	keptnv2 "github.com/keptn/go-utils/pkg/lib/v0_2_0"
 	log "github.com/sirupsen/logrus"
+
+	"github.com/keptn-contrib/dynatrace-service/internal/dynatrace"
+	"github.com/keptn-contrib/dynatrace-service/internal/keptn"
 )
 
 type ReleaseTriggeredEventHandler struct {
@@ -29,7 +30,7 @@ func NewReleaseTriggeredEventHandler(event ReleaseTriggeredAdapterInterface, dtC
 }
 
 // HandleEvent handles an action finished event.
-func (eh *ReleaseTriggeredEventHandler) HandleEvent(workCtx context.Context, replyCtx context.Context) error {
+func (eh *ReleaseTriggeredEventHandler) HandleEvent(workCtx context.Context, _ context.Context) error {
 	strategy, err := keptnevents.GetDeploymentStrategy(eh.event.GetDeploymentStrategy())
 	if err != nil {
 		log.WithError(err).Error("Could not determine deployment strategy")
@@ -60,6 +61,10 @@ func (eh *ReleaseTriggeredEventHandler) getTitle(strategy keptnevents.Deployment
 		}
 
 		return fmt.Sprintf("NOT PROMOTING from %s to next stage", eh.event.GetStage())
+	}
+
+	if defaultValue == "" {
+		return fmt.Sprintf("Release triggered for %s, %s and %s", eh.event.GetProject(), eh.event.GetStage(), eh.event.GetService())
 	}
 
 	return defaultValue
