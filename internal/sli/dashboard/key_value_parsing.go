38ed2dd6d8de4a0db8a25aa82dfcdbd4db2297e8
Arthur Pitman <arthur.pitman@dynatrace.com>
2022-07-04 14:28:46 +0200

    Use shared key value parsing
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>

--- internal/sli/dashboard/key_value_parsing.go
+++ internal/sli/dashboard/key_value_parsing.go
@@ -1,6 +1,41 @@
 package dashboard
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+type keyValue struct {
+	key   string
+	value string
+	split bool
+}
+
+type keyValueParsing struct {
+	value string
+}
+
+func newKeyValueParsing(value string) *keyValueParsing {
+	return &keyValueParsing{value: value}
+}
+
+func (p *keyValueParsing) parse() []keyValue {
+	pairs := []keyValue{}
+
+	pieces := strings.Split(p.value, ";")
+	for _, pair := range pieces {
+		pair = strings.TrimSpace(pair)
+		key, value, found := strings.Cut(pair, "=")
+
+		pairs = append(pairs, keyValue{
+			key:   strings.TrimSpace(key),
+			value: strings.TrimSpace(value),
+			split: found,
+		})
+	}
+
+	return pairs
+}
 
 type duplicateKeyError struct {
 	key string
