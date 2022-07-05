29ce9071dda4e4e54c8363e2f78e2c075bdb8a26
Arthur Pitman <arthur.pitman@dynatrace.com>
2022-07-05 13:05:51 +0200

    Replace `$` and `.` with `_` in indicator names
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>

--- internal/common/common.go
+++ internal/common/common.go
@@ -72,11 +72,13 @@ func TimestampToUnixMillisecondsString(time time.Time) string {
 	return strconv.FormatInt(time.Unix()*1000, 10)
 }
 
-// CleanIndicatorName makes sure we have a valid indicator name by getting rid of special characters
+// CleanIndicatorName makes sure we have a valid indicator name by getting rid of special characters.
+// All spaces, periods, forward-slashs, and percent and dollar signs are replaced with an underscore.
 func CleanIndicatorName(indicatorName string) string {
 	indicatorName = strings.ReplaceAll(indicatorName, " ", "_")
 	indicatorName = strings.ReplaceAll(indicatorName, "/", "_")
 	indicatorName = strings.ReplaceAll(indicatorName, "%", "_")
-
+	indicatorName = strings.ReplaceAll(indicatorName, "$", "_")
+	indicatorName = strings.ReplaceAll(indicatorName, ".", "_")
 	return indicatorName
 }
