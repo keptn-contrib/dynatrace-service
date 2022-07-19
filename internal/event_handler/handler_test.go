5ebc4d97ca4fb43165085eda47f4f4756c3f9220
Arthur Pitman <arthur.pitman@dynatrace.com>
2022-07-19 13:00:59 +0200

    Add test
    Signed-off-by: Arthur Pitman <arthur.pitman@dynatrace.com>

--- internal/event_handler/handler_test.go
+++ internal/event_handler/handler_test.go
@@ -1,10 +1,13 @@
 package event_handler
 
 import (
+	"context"
 	"net/url"
 	"testing"
 
 	cloudevents "github.com/cloudevents/sdk-go/v2"
+	"github.com/keptn-contrib/dynatrace-service/internal/adapter"
+	"github.com/keptn-contrib/dynatrace-service/internal/keptn"
 	"github.com/keptn-contrib/dynatrace-service/internal/sli"
 	keptnv2 "github.com/keptn/go-utils/pkg/lib/v0_2_0"
 	"github.com/stretchr/testify/assert"
@@ -43,6 +46,30 @@ func Test_getEventAdapterForGetSLITriggeredNotForDynatrace(t *testing.T) {
 	assert.Nil(t, adapter)
 }
 
+// TestEventHandlerIgnoresGetSLITriggeredNotForDynatrace tests that EventHandler ignores "sh.keptn.event.get-sli.triggered" events with an SLIProvider other than "dynatrace".
+func TestEventHandlerIgnoresGetSLITriggeredNotForDynatrace(t *testing.T) {
+	getSLITriggeredEvent, err := createTestGetSLITriggeredCloudEvent("other")
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	clientFactory := &clientFactoryMock{
+		t: t,
+	}
+
+	eventSenderClient := &eventSenderClientMock{
+		t: t,
+	}
+
+	handler, err := NewEventHandler(context.Background(), clientFactory, eventSenderClient, getSLITriggeredEvent)
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	err = handler.HandleEvent(context.Background(), context.Background())
+	assert.NoError(t, err)
+}
+
 func createTestGetSLITriggeredCloudEvent(sliProvider string) (cloudevents.Event, error) {
 	return createTestCloudEvent("sh.keptn.event.get-sli.triggered", keptnv2.GetSLITriggeredEventData{
 		EventData: keptnv2.EventData{
@@ -72,4 +99,39 @@ func createTestCloudEvent(eventType string, payload any) (cloudevents.Event, err
 	err := ev.SetData(cloudevents.ApplicationJSON, payload)
 
 	return ev, err
+}
+
+// eventSenderClientMock is an implementation of keptn.EventSenderClientInterface whose methods panic if called.
+type eventSenderClientMock struct {
+	t *testing.T
+}
+
+func (m *eventSenderClientMock) SendCloudEvent(factory adapter.CloudEventFactoryInterface) error {
+	m.t.Fatalf("SendCloudEvent() should not be needed in this mock!")
+	return nil
+}
+
+// clientFactoryMock is an implementation of keptn.ClientFactoryInterface whose methods panic if called.
+type clientFactoryMock struct {
+	t *testing.T
+}
+
+func (m *clientFactoryMock) CreateEventClient() keptn.EventClientInterface {
+	m.t.Fatalf("SendCloudEvent() should not be needed in this mock!")
+	return nil
+}
+
+func (m *clientFactoryMock) CreateResourceClient() keptn.ResourceClientInterface {
+	m.t.Fatalf("SendCloudEvent() should not be needed in this mock!")
+	return nil
+}
+
+func (m *clientFactoryMock) CreateServiceClient() keptn.ServiceClientInterface {
+	m.t.Fatalf("SendCloudEvent() should not be needed in this mock!")
+	return nil
+}
+
+func (m *clientFactoryMock) CreateUniformClient() keptn.UniformClientInterface {
+	m.t.Fatalf("SendCloudEvent() should not be needed in this mock!")
+	return nil
 }
